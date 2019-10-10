3bfa427591b5c9f01b00a8616a018583c325ae5f
DELL <yevhenii@rubygarage.org>
2019-10-10 12:04:05 +0300

    Refactored

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -20,63 +20,93 @@ require 'pry'
 # More scoring examples are given in the tests below:
 #
 # Your goal is to write the score method.
+class Test
+  def initialize
+    @result = 0
+  end
 
-def score(dice)
-  score = 0
+  def score(dice)
+    return @result if dice.empty?
 
-  1.upto(6).each do |num_rolled|
-    amount = dice.count(num_rolled)
+    @test = dice.group_by(&:itself)
+    handle_thousand
+    handle_set
+    handle_five
+    handle_one
+    @result
+  end
 
-    if amount >= 3
-      score += num_rolled == 1 ? 1000 : num_rolled * 100
-      amount -= 3
+  private
+
+  def handle_thousand
+    return unless @test[1]&.length.to_i > 2
+
+    @result += 1000
+    @test[1].slice!(0, 3)
+  end
+
+  def handle_set
+    @test.each do |num, qunatity|
+      next unless qunatity.length > 2
+
+      @result += num * 100
+      @test[num].slice!(0, 3)
     end
+  end
 
-    score += 100 * amount if num_rolled == 1
-    score += 50 * amount if num_rolled == 5
-  end     
-  score
+  def handle_five
+    return unless @test[5]&.any?
+
+    @result += 50 * @test[5].length
+  end
+
+  def handle_one
+    return unless @test[1]&.any?
+
+    @result += 100 * @test[1].length
+  end
 end
+
 
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
-    assert_equal 0, score([])
+    assert_equal 0, Test.new.score([])
   end
 
   def test_score_of_a_single_roll_of_5_is_50
-    assert_equal 50, score([5])
+    assert_equal 50, Test.new.score([5])
   end
 
   def test_score_of_a_single_roll_of_1_is_100
-    assert_equal 100, score([1])
+    assert_equal 100, Test.new.score([1])
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
+    assert_equal 300, Test.new.score([1,5,5,1])
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
+    assert_equal 0, Test.new.score([2,3,4,6])
   end
 
   def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
+    assert_equal 1000, Test.new.score([1,1,1])
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2,2,2])
-    assert_equal 300, score([3,3,3])
-    assert_equal 400, score([4,4,4])
-    assert_equal 500, score([5,5,5])
-    assert_equal 600, score([6,6,6])
+    assert_equal 200, Test.new.score([2,2,2])
+    assert_equal 300, Test.new.score([3,3,3])
+    assert_equal 400, Test.new.score([4,4,4])
+    assert_equal 500, Test.new.score([5,5,5])
+    assert_equal 600, Test.new.score([6,6,6])
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2,5,2,2,3])
-    assert_equal 550, score([5,5,5,5])
-    assert_equal 1100, score([1,1,1,1])
-    assert_equal 1200, score([1,1,1,1,1])
-    assert_equal 1150, score([1,1,1,5,1])
+    assert_equal 250, Test.new.score([2,5,2,2,3])
+    assert_equal 550, Test.new.score([5,5,5,5])
+    assert_equal 1100, Test.new.score([1,1,1,1])
+    assert_equal 1200, Test.new.score([1,1,1,1,1])
+    assert_equal 1150, Test.new.score([1,1,1,5,1])
   end
 
 end
